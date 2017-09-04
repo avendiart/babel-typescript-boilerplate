4f7fa25522afbd319b3cad70f55402d3cd0e8f7a
Vladislav Pilgasov <avendiart@gmail.com>
2017-09-04 18:32:19 +0200

    Improve jest configuration and update packages

--- transform.js
+++ transform.js
@@ -1,23 +1,5 @@
 const babel = require('babel-core');
 
-const options = {
-  "sourceMaps": "inline",
-  "presets": [
-    [
-      "env", {
-        "targets": {
-          "node": true,
-        },
-      },
-    ],
-    "stage-3",
-    "stage-2",
-    "stage-1",
-    "stage-0",
-    "typescript",
-  ],
-};
-
 module.exports = {
-  process: (code) => babel.transform(code, options).code,
+  process: (code, path) => babel.transform(code, { retainLines: true, filename: path }),
 };
